082f97069897fc7322d493dde16680679228e100
H. Turgut Uyar <uyar@tekir.org>
2018-11-20 18:35:35 +0300

    Add adapters for lektor url filters

--- lektor_chameleon.py
+++ lektor_chameleon.py
@@ -5,6 +5,11 @@ from lektor.pluginsystem import Plugin
 def render_template(self, name, pad=None, this=None, values=None, alt=None):
     ctx = self.make_default_tmpl_values(pad, this, values, alt, template=name)
     ctx.update(self.jinja_env.globals)
+
+    filters = self.jinja_env.filters
+    ctx["asseturl"] = lambda *a, **kw: filters["asseturl"](ctx, *a, **kw)
+    ctx["url"] = lambda *a, **kw: filters["url"](ctx, *a, **kw)
+
     template = self.chameleon_loader[name]
     return template(**ctx)
 
