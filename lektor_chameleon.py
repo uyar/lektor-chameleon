3c5e3034e65622a2be08a2c2dffe9f1521672030
H. Turgut Uyar <uyar@tekir.org>
2018-11-21 23:01:36 +0300

    Make filters work with markup objects

--- lektor_chameleon.py
+++ lektor_chameleon.py
@@ -2,20 +2,23 @@ from functools import partial
 
 from chameleon import PageTemplateLoader
 from lektor.pluginsystem import Plugin
+from markupsafe import Markup
 
 
 class Filter:
-    def __init__(self, func, ctx=None):
+    def __init__(self, func, ctx=None, markup=False):
         self.func = func if ctx is None else partial(func, ctx)
+        self.markup = markup
 
     def __call__(self, **kwargs):
-        return Filter(partial(self.func, **kwargs))
+        return Filter(partial(self.func, **kwargs), markup=self.markup)
 
     def __rrshift__(self, x):
-        return self.func(x)
+        return self.func(x) if not self.markup else Markup(self.func(x.html).unescape())
 
 
 _CONTEXT_FILTERS = {"url", "asseturl", "markdown"}
+_MARKUP_FILTERS = {"indent"}
 
 
 def render_template(self, name, pad=None, this=None, values=None, alt=None):
@@ -23,7 +26,11 @@ def render_template(self, name, pad=None, this=None, values=None, alt=None):
     ctx.update(self.jinja_env.globals)
 
     for f_name, f_func in self.jinja_env.filters.items():
-        ctx[f_name] = Filter(f_func, ctx=ctx if f_name in _CONTEXT_FILTERS else None)
+        ctx[f_name] = Filter(
+            f_func,
+            ctx=ctx if f_name in _CONTEXT_FILTERS else None,
+            markup=f_name in _MARKUP_FILTERS,
+        )
 
     template = self.chameleon_loader[name]
     return template(**ctx)
