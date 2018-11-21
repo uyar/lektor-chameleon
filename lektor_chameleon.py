c6a8a2483b4fcdb4562f7f0a3e368a66e2a8e650
H. Turgut Uyar <uyar@tekir.org>
2018-11-21 22:28:50 +0300

    Generalize jinja filter adaptation

--- lektor_chameleon.py
+++ lektor_chameleon.py
@@ -7,20 +7,23 @@ from lektor.pluginsystem import Plugin
 class Filter:
     def __init__(self, func, ctx=None):
         self.func = func if ctx is None else partial(func, ctx)
- 
+
     def __call__(self, **kwargs):
         return Filter(partial(self.func, **kwargs))
- 
+
     def __rrshift__(self, x):
         return self.func(x)
+
+
+_CONTEXT_FILTERS = {"url", "asseturl", "markdown"}
 
 
 def render_template(self, name, pad=None, this=None, values=None, alt=None):
     ctx = self.make_default_tmpl_values(pad, this, values, alt, template=name)
     ctx.update(self.jinja_env.globals)
 
-    ctx["asseturl"] = Filter(self.jinja_env.filters["asseturl"], ctx)
-    ctx["url"] = Filter(self.jinja_env.filters["url"], ctx)
+    for f_name, f_func in self.jinja_env.filters.items():
+        ctx[f_name] = Filter(f_func, ctx=ctx if f_name in _CONTEXT_FILTERS else None)
 
     template = self.chameleon_loader[name]
     return template(**ctx)
