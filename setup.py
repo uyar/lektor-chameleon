ca3a9e3705a96e3d0e61630d94466bbc54e4ffa3
H. Turgut Uyar <uyar@tekir.org>
2018-11-28 17:06:30 +0300

    Update package metadata

--- setup.py
+++ setup.py
@@ -1,15 +1,27 @@
 from setuptools import setup
 
 
+with open("README.rst") as readme_file:
+    readme = readme_file.read()
+
 setup(
     name="lektor-chameleon",
-    description="Chameleon support for templating.",
     version="0.1",
+    description="Chameleon support for templating in Lektor.",
+    long_description=readme,
+    url="http://github.com/uyar/lektor-chameleon",
     author="H. Turgut Uyar",
     author_email="uyar@tekir.org",
-    url="http://github.com/uyar/lektor-chameleon",
     license="LGPL",
-    install_requires=["chameleon"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Environment :: Plugins",
+        "Framework :: Lektor",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
+    ],
+    keywords="lektor chameleon templating",
     py_modules=["lektor_chameleon"],
+    install_requires=["chameleon"],
     entry_points={"lektor.plugins": ["chameleon = lektor_chameleon:ChameleonPlugin"]},
 )
