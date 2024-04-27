44280fcbf8b926c6420150c2b6105eb06712f8d7
H. Turgut Uyar <uyar@tekir.org>
2024-04-27 21:58:41 +0300

    Add basic tests

--- tests/test_chameleon.py
+++ tests/test_chameleon.py
@@ -1,7 +1,68 @@
+import os
+import subprocess
 from importlib import metadata
+from pathlib import Path
+from shutil import copytree, rmtree
+
+from pytest import fixture
 
 import lektor_chameleon
 
 
+LEKTOR_SRC_ROOT = Path(__file__).parent / "project"
+LEKTOR_ROOT = Path("/dev/shm/lektor-chameleon")
+LEKTOR_BUILD_ROOT = LEKTOR_ROOT / "_build"
+LEKTOR_BUILD_INDEX = LEKTOR_BUILD_ROOT / "index.html"
+
+
+@fixture(autouse=True)
+def lektor_init():
+    rmtree(LEKTOR_ROOT, ignore_errors=True)
+    copytree(LEKTOR_SRC_ROOT, LEKTOR_ROOT)
+
+    current_dir = os.getcwd()
+    os.chdir(LEKTOR_ROOT)
+    yield
+
+    os.chdir(current_dir)
+    rmtree(LEKTOR_ROOT)
+
+
 def test_installed_version_should_match_tested_version():
     assert metadata.version("lektor_chameleon") == lektor_chameleon.__version__
+
+
+def test_tal_content_should_replace_content():
+    subprocess.run(["lektor", "build"])
+    output = LEKTOR_BUILD_INDEX.read_text()
+    assert '<main><p>Page content.</p>\n</main>' in output
+
+
+def test_interpolation_should_replace_content():
+    subprocess.run(["lektor", "build"])
+    output = LEKTOR_BUILD_INDEX.read_text()
+    assert '<title>Page title</title>' in output
+
+
+def test_context_filter_should_work():
+    subprocess.run(["lektor", "build"])
+    output = LEKTOR_BUILD_INDEX.read_text()
+    assert '<link rel="stylesheet" href="static/style.css"/>' in output
+
+
+def test_str_filter_should_work():
+    subprocess.run(["lektor", "build"])
+    output = LEKTOR_BUILD_INDEX.read_text()
+    assert '<meta name="keywords" content="chameleon"/>' in output
+
+
+def test_str_filter_with_parameters_should_work():
+    subprocess.run(["lektor", "build"])
+    output = LEKTOR_BUILD_INDEX.read_text()
+    assert '<h1>  Page title  </h1>' in output
+
+
+def test_jinja_env_filter_should_work():
+    subprocess.run(["lektor", "build"])
+    output = LEKTOR_BUILD_INDEX.read_text()
+    assert '<footer>Page\ntitle</footer>' in output
